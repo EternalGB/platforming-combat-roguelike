034fe8e68a5b8ccc9068aedac460b890dc263918
= <raph.byrne@gmail.com>
2014-06-24 17:53:53 +0800

    added a passive to DOTBlast

--- Assets/Scripts/Abilities/Blasts/DOTBlast.cs
+++ Assets/Scripts/Abilities/Blasts/DOTBlast.cs
@@ -5,6 +5,8 @@ public class DOTBlast : CloseBlast
 {
 	
 	public int intervals;
+	public LayerMask onHitPassiveTargets;
+	int passiveActionID = 0;
 
 	override public void burstEffect(Transform blast, Transform target)
 	{
@@ -16,12 +18,20 @@ public class DOTBlast : CloseBlast
 
 	override public void applyPassive(Transform player)
 	{
-		
+		ActionOnHit action = player.gameObject.AddComponent<ActionOnHit>();
+		action.init(burstEffect,onHitPassiveTargets,cooldown);
+		passiveActionID = action.GetInstanceID();
 	}
 	
 	override public void undoPassive(Transform player)
 	{
-		
+		ActionOnHit[] actions = player.gameObject.GetComponents<ActionOnHit>();
+		ActionOnHit action = null;
+		foreach(ActionOnHit a in actions)
+			if(a.GetInstanceID() == passiveActionID)
+				action = a;
+		if(action)
+			Destroy(action);
 	}
 
 
