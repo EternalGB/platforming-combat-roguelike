2b6e1c0f34a5c92eec562e7cc2fcbf05afa87026
RaphByrne <raph.byrne@gmail.com>
2014-08-08 12:55:01 +0800

    added the completely random level generator

--- Assets/Scripts/LevelGeneration/RandomLevelConstructor.cs
+++ Assets/Scripts/LevelGeneration/RandomLevelConstructor.cs
@@ -4,9 +4,37 @@ using System.Collections;
 public class RandomLevelConstructor : MonoBehaviour
 {
 
+	public int width;
+	public int height;
+	public string tilePath;
+	public float tileWidth;
+	public Vector3 startingPos;
+
+
+	string path = "RoomGeneration/Rooms/";
+
 	void Start()
 	{
+		TextAsset[] textRooms = Resources.LoadAll<TextAsset>(path);
+		Room[] possibleRooms = new Room[textRooms.Length];
+		for(int i = 0; i < textRooms.Length; i++) {
+			possibleRooms[i] = new Room(textRooms[i]);
+		}
 
+		//all rooms have to be the same width and height
+		float roomWidth = possibleRooms[0].Width;
+		float roomHeight = possibleRooms[0].Height;
+		for(int i = 0; i < width; i++) {
+			for(int j = 0; j < height; j++) {
+				Room nextRoom = possibleRooms[Random.Range(0,possibleRooms.Length-1)];
+				if(nextRoom.Width == roomWidth && nextRoom.Height == roomHeight)
+					nextRoom.construct(tilePath,tileWidth,startingPos + 
+					                   new Vector3(i*roomWidth*tileWidth,-j*roomHeight*tileWidth));
+				else
+					Debug.LogError("Room " + nextRoom.name + " does not have the right dimensions");
+
+			}
+		}
 	}
 
 }
